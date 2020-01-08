b32eeda1c9b3b0aa2c17f3ad3bc085ca8d51c016
Johan B.W. de Vries <jbwdevries@gmail.com>
2020-01-08 16:52:28 +0100

    Fix: Always try to import pytest_timout
    Even when not available.

--- pytest_integration/pytest_plugin.py
+++ pytest_integration/pytest_plugin.py
@@ -5,10 +5,19 @@ or slow integration tests.
 See https://github.com/jbwdevries/pytest-integration
 """
 
-import warnings
-
 import pytest
-import pytest_timeout
+
+try:
+    import pytest_cov # pylint: disable=W0611
+    HAVE_PYTEST_COV = True
+except ImportError:
+    HAVE_PYTEST_COV = False
+
+try:
+    import pytest_timeout
+    HAVE_PYTEST_TIMEOUT = True
+except ImportError:
+    HAVE_PYTEST_TIMEOUT = False
 
 def pytest_load_initial_conftests(early_config, parser, args):
     """
@@ -30,11 +39,14 @@ def pytest_load_initial_conftests(early_config, parser, args):
         '--without-slow-integration', action='store_const', const=False,
         dest='run_slow_integration')
 
-    parser.addoption('--integration-cover', action='store_true')
-    parser.addoption('--integration-timeout', type=int, default=0)
-    parser.addoption(
-        '--integration-timeout-method',
-        type=_timeout_method, default='DEFAULT_METHOD')
+    if HAVE_PYTEST_COV:
+        parser.addoption('--integration-cover', action='store_true')
+
+    if HAVE_PYTEST_TIMEOUT:
+        parser.addoption('--integration-timeout', type=int, default=0)
+        parser.addoption(
+            '--integration-timeout-method',
+            type=_timeout_method, default='DEFAULT_METHOD')
 
 def pytest_configure(config):
     """
@@ -63,22 +75,12 @@ def pytest_collection_modifyitems(session, config, items):
     items.sort(key=_get_items_key)
 
     no_cover_marker = None
-    if not config.getoption('integration_cover'):
-        with warnings.catch_warnings():
-            warnings.filterwarnings('error')
-            try:
-                no_cover_marker = pytest.mark.no_cover
-            except Warning:
-                pass
+    if HAVE_PYTEST_COV and not config.getoption('integration_cover'):
+        no_cover_marker = pytest.mark.no_cover
 
     timeout_marker = None
-    if config.getoption('integration_timeout') > 0:
-        with warnings.catch_warnings():
-            warnings.filterwarnings('error')
-            try:
-                timeout_marker = pytest.mark.timeout
-            except Warning:
-                pass
+    if HAVE_PYTEST_TIMEOUT and config.getoption('integration_timeout') > 0:
+        timeout_marker = pytest.mark.timeout
 
     if not no_cover_marker and not timeout_marker:
         # pytest-cov and pytest-timeout not installed, or disabled
@@ -145,7 +147,7 @@ def _get_items_key(item):
     return 0
 
 def _timeout_method(item):
-    if item == 'DEFAULT_METHOD':
+    if HAVE_PYTEST_TIMEOUT and item == 'DEFAULT_METHOD':
         return pytest_timeout.DEFAULT_METHOD
 
     if item not in ['thread', 'signal']:
